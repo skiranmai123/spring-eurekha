a3b25e2f2b5c176debe2a5b029a3e591bbc19aac
kisuram <kiranmaisuram2@gmail.com>
2019-10-11 13:25:57 -0700

    spring-12345[kiranmai]successfully completed

---
+++ src/main/java/com/cloudproject/cloudproject/CloudprojectApplication.java
@@ -0,0 +1,13 @@
+package com.cloudproject.cloudproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudprojectApplication.class, args);
+	}
+
+}
